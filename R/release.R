3302e14d1dfab421843045a5c3eb61b8628a9757
Xu Fei <xu@a2-ai.com>
2025-01-30 22:50:34 +0000

    Add ... to allow flag passthrough

--- R/release.R
+++ R/release.R
@@ -1,29 +1,32 @@
 #' create a cranlike repo that contains the tagged package
 #' @param .dir directory to create the repo in
 #' @param repo_name repository name to specify in the description
+#' @param ... additional arguments passed to build_pkg
 #' @export
-create_tagged_repo <- function(.dir, repo_name = "pkgpub") {
+create_tagged_repo <- function(.dir, repo_name = "pkgpub", ...) {
   ctag <- current_commit_tag()
-
+  
   # not tagged
   if (is.null(ctag)) {
-   stop("current commit is not tagged")
+    stop("current commit is not tagged")
   }
-  desc <- as.data.frame(read.dcf("DESCRIPTION",fields = "Version"))
+  desc <- as.data.frame(read.dcf("DESCRIPTION", fields = "Version"))
   if (desc$Version != ctag) {
     stop(sprintf("mismatch between git tag (%s) and DESCRIPTION Version (%s)", ctag, desc$Version), call. = FALSE)
   }
-
+  
   repo_dir <- new_cranlike_repo(file.path(.dir, ctag))
-
-  built_dir <- build_pkg(.pkgdir = ".",
-                         types = "source",
-                         origin = sanitize_git_url(git_url()),
-                         repository = repo_name,
-                         addl_meta = list(git_tag = ctag, git_commit = gert::git_info()$commit),
-                         supplement_version = FALSE
-                         )
-
+  
+  built_dir <- build_pkg(
+    .pkgdir = ".",
+    types = "source",
+    origin = sanitize_git_url(git_url()),
+    repository = repo_name,
+    addl_meta = list(git_tag = ctag, git_commit = gert::git_info()$commit),
+    supplement_version = FALSE,
+    ...
+  )
+  
   res <- insert_packages(unlist(built_dir), repo_dir)
   if (!isTRUE(res)) {
     rlang::abort("failure building or inserting package")
