8ca9b5b0ac2cef6fa744ed18d89b8a891fbce665
Devin <devin@a2-ai.com>
2025-01-30 01:43:28 +0000

    add remotes test

--- tests/testthat/test-modify-description.R
+++ tests/testthat/test-modify-description.R
@@ -6,6 +6,16 @@ test_that("modify description will delete a remotes field if requested", {
   output_file <- tempfile()
   modify_desc(d, list(Repository = "test"), output_file) 
   expect_snapshot_file(output_file, "simple.DESCRIPTION")
+})
+
+test_that("modify description will delete a remotes field if requested", {
+  # description should still work if no remotes field present
+  d <- desc::desc(file = "./testdata/remotes.DESCRIPTION")
+  expect_equal(as.character(d$get("Package")), "test")
+  expect_equal(d$get_remotes(), "rstudio/gt#4")
+  output_file <- tempfile()
+  modify_desc(d, list(), output_file, remove_remotes = TRUE) 
+  expect_snapshot_file(output_file, "remove-remote.DESCRIPTION")
   # description should remove the remotes field if it was present and everything
   # else should be unchanged
 })
